cbe5cd240eb26573b94d34ad709eb3fbb9ef2963
Ben Joseph <benjosephcr7@gmail.com>
2024-06-25 20:14:47 +0530

    changes related to app launch first time in progress

---
+++ AssetManagement/AssetManagement/Common/CommonFunctions.cs
@@ -0,0 +1,44 @@
+﻿using AssetManagement.Models;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssetManagement.Common
+{
+    public class CommonFunctions
+    {
+        private SQLiteAsyncConnection _dbConnection;
+        public void CommonDataForDefaults()
+        {
+            AddIncomeExpenseCategoryDefaults();
+            AddCurrencyListDefaults();
+            AddUserCurrencyDefault();
+        }
+        public void AddIncomeExpenseCategoryDefaults()
+        {
+
+        }
+
+        public async void AddCurrencyListDefaults()
+        {
+            List<Currency> currencyList = new List<Currency>
+            {
+               new Currency { Country="India",CurrencyName = "INR",CurrencyCode="" },
+               new Currency { Country="United States",CurrencyName = "USD",CurrencyCode="" },
+               new Currency { Country="England",CurrencyName = "GBP",CurrencyCode="" },
+               new Currency { Country="Germany",CurrencyName = "UERO",CurrencyCode="" },
+               new Currency { Country="Singapore",CurrencyName = "SGD",CurrencyCode="" }
+            };
+
+            await _dbConnection.InsertAllAsync(currencyList);
+        }
+
+        public void AddUserCurrencyDefault()
+        {
+
+        }
+    }
+}
--- AssetManagement/AssetManagement/Views/AppLaunchPage.xaml.cs
+++ AssetManagement/AssetManagement/Views/AppLaunchPage.xaml.cs
@@ -1,14 +1,17 @@
 using AndroidX.Lifecycle;
+using AssetManagement.Common;
+using AssetManagement.Models;
 using AssetManagement.Models.Constants;
 using AssetManagement.Services;
 using AssetManagement.ViewModels;
+using SQLite;
 
 namespace AssetManagement.Views;
 
 public partial class AppLaunchPage : ContentPage
 {
     private AssetListPageViewModel _viewModel;
-    //private IPopupNavigation _popupNavigation;
+    private SQLiteAsyncConnection _dbConnection;
     private readonly IAssetService _assetService;
     public readonly string _from;
     public AppLaunchPage(AssetListPageViewModel viewModel, IAssetService assetService,string from)
@@ -19,12 +22,96 @@ public partial class AppLaunchPage : ContentPage
         _from = from;
     }
 
-    private void Button_Clicked(object sender, EventArgs e)
+    protected override void OnAppearing()
+    {
+        try
+        {
+            base.OnAppearing();
+
+            CommonFunctions objCommon = new CommonFunctions();
+            objCommon.CommonDataForDefaults();
+            LoadCurrenctListInDropdown();
+        }
+        catch (Exception)
+        {
+            //await DisplayAlert("Error", ex.Message, "OK");
+            return;
+        }
+    }
+
+    private async Task SetUpDb()
+    {
+        try
+        {
+            if (_dbConnection == null)
+            {
+                string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Assets.db3");
+                _dbConnection = new SQLiteAsyncConnection(dbPath);               
+                await _dbConnection.CreateTableAsync<Assets>();
+                await _dbConnection.CreateTableAsync<IncomeExpenseModel>();
+                await _dbConnection.CreateTableAsync<IncomeExpenseCategories>();
+                await _dbConnection.CreateTableAsync<DataSyncAudit>();
+                await _dbConnection.CreateTableAsync<AssetAuditLog>();
+                await _dbConnection.CreateTableAsync<Owners>();
+                await _dbConnection.CreateTableAsync<UserCurrency>();
+                await _dbConnection.CreateTableAsync<Currency>();
+            }
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Error", ex.Message, "OK");
+        }
+    }
+
+    private async void LoadCurrenctListInDropdown()
+    {
+        try
+        {
+            await SetUpDb();
+            var currencyList = await _dbConnection.Table<Currency>().ToListAsync();
+            //IncomeExpenseCategories objCategories = new IncomeExpenseCategories
+            //{
+            //    IncomeExpenseCategoryId = 0,
+            //    CategoryName = Constants.AddNewCategoryOption,
+            //    CategoryType = "Expense"
+            //};
+            //expenseCategories.Add(objCategories);
+            pickerCurrencyList.ItemsSource = currencyList.Select(c => c.CurrencyName).ToList();
+            //if (expenseCategories.Count > 1)
+            //{
+            //    pickerExpenseCategory.SelectedIndex = 0;
+            //}
+        }
+        catch (Exception)
+        {
+            return;
+        }
+    }
+
+    private async void Done_Clicked(object sender, EventArgs e)
     {
         if (_from == Constants.FromLaunchPage)
         {
-            Constants.SetCurrency("en-US");
+            //add user currency into table
+            await SetUpDb();
+            UserCurrency userCurrency = new UserCurrency
+            {
+                Country = "",
+                CurrencyName = "",
+                CurrencyCode = ""
+            };
+            await _dbConnection.InsertAsync(userCurrency);
+            //add user currency into table
+
+           
+
+            //Constants.SetCurrency("en-US");
             Application.Current.MainPage = new AppFlyoutPage(_viewModel, _assetService);
         }
+    }
+
+    private void pickerCurrencyList_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
     }
 }
